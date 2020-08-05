36085789c131842421715aa89478a8b1acea30ac
burinson <garentei@gmail.com>
2020-08-04 19:38:43 -0500

    changed <typedef> for <using> in template

--- ICPC/Codes/temShort.cpp
+++ ICPC/Codes/temShort.cpp
@@ -16,10 +16,10 @@ using namespace std;
 #define s second
 #define pb push_back
 
-typedef long long lli;
-typedef long double ld;
-typedef pair<int, int> ii;
-typedef vector<int> vi;
+using lli = long long;
+using ld = long double;
+using ii = pair<int, int>;
+using vi = vector<int>;
 
 int main() {
   cin.tie(0)->sync_with_stdio(0), cout.tie(0);
