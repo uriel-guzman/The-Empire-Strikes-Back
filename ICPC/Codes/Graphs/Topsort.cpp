88b198dc098bb787df631debf52972672282c42b
Abraham Murillo <abraham@Abrahams-Macbook-Air.local>
2020-08-08 17:25:28 -0500

    topsort correction

--- ICPC/Codes/Graphs/Topsort.cpp
+++ ICPC/Codes/Graphs/Topsort.cpp
@@ -15,19 +15,3 @@ void topsort() { // first fill the indeg[]
         qu.push(v);
   }
 }
-
-
-vi order;
-int indeg[N];
-
-void topsort() { // first fill the indeg[]
-  fore (u, 1, n + 1)
-    if (indeg[u] == 0)
-      order.pb(u);
-  fore (i, 0, sz(order)) {
-    int u = order[i];
-    for (int v : graph[u])
-      if (--indeg[v] == 0) 
-        order.pb(v);
-  }
-}
