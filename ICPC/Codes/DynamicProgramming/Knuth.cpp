8e84cbffb18750e77f8916742f1b42859539affb
burinson <garentei@gmail.com>
2020-08-09 15:04:04 -0500

    Knuth optimization :D

---
+++ ICPC/Codes/DynamicProgramming/Knuth.cpp
@@ -0,0 +1,22 @@
+lli dp[N][N];
+int opt[N][N];
+
+fore (len, 1, n + 1) 
+  fore (l, 0, n) {
+    int r = l + len - 1;
+    if (r > n - 1)
+      break;
+    if (len <= 2) {
+      dp[l][r] = 0;
+      opt[l][r] = l;
+      continue;
+    }
+    dp[l][r] = inf;
+    fore (k, opt[l][r - 1], opt[l + 1][r] + 1) {
+      lli cur = dp[l][k] + dp[k][r] + cost;
+      if (cur < dp[l][r]) {
+        dp[l][r] = cur;
+        opt[l][r] = k;
+      }
+    }
+  }
