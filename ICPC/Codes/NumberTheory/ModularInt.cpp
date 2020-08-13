5ecf0614d7b8b28583a790f0a35a255c8fc0ecc9
Abraham Murillo <abraham@Abrahams-Macbook-Air.local>
2020-08-13 11:05:52 -0500

    Modular int with modular inverse and the possibility of be read

--- ICPC/Codes/NumberTheory/ModularInt.cpp
+++ ICPC/Codes/NumberTheory/ModularInt.cpp
@@ -2,11 +2,23 @@ template <int mod>
 struct Modular {
   int v;
   Modular(lli x = 0) : v(x % mod) {}
+
   Modular operator + (const Modular &m) const { return Modular(v + m.v); }
   Modular operator - (const Modular &m) const { return Modular(v - m.v + mod); }
   Modular operator * (const Modular &m) const { return Modular(1LL * v * m.v); }
+  Modular inv() const { return Modular(minv(v, mod)); } 
+  
   void operator += (const Modular &m) { *this = *this + m; }
   void operator -= (const Modular &m) { *this = *this - m; }
   void operator *= (const Modular &m) { *this = *this * m; }
+  
   friend ostream & operator << (ostream &os, const Modular &m) { return os << m.v; }
+  friend istream & operator << (istream &os, const Modular &m) { return is >> m.v; }
+  
+private:
+  static int minv(int a, int m) {
+		a %= m;
+		assert(a); // ensures this is valid
+		return a == 1 ? 1 : int(m - lli(minv(m, a)) * lli(m) / a);
+	}
 };
