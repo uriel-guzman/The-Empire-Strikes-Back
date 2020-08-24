a38250a668eba91f875667b09bf787abf23bc9b4
Abraham Murillo <abraham@Abrahams-Macbook-Air.local>
2020-08-24 10:56:58 -0500

    Update of The book

--- ICPC/Codes/random.cpp
+++ ICPC/Codes/random.cpp
@@ -1,4 +1,2 @@
-uint32_t rng(uint32_t l = 0, uint32_t r = numeric_limits<uint32_t>::max()) {
-  static mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
-  return uniform_int_distribution<uint32_t>(l, r)(gen);
-}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<>(l, r)(rng);
