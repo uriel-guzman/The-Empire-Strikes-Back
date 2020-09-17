3b425becc54577e90c0db18e049b07f2591f7ed3
burinson <garentei@gmail.com>
2020-09-17 15:05:45 -0500

    real.cpp :c

--- ICPC/Codes/real.cpp
+++ ICPC/Codes/real.cpp
@@ -3,5 +3,5 @@ const ld eps = 1e-9;
 #define neq(a, b) !eq(a, b) 
 #define geq(a, b) (a) - (b) >= eps
 #define leq(a, b) (a) - (b) <= eps
-#define ge(a, b) !leq(a, b) 
-#define le(a, b) !geq(a, b) 
+#define ge(a, b) (a) - (b) > eps
+#define le(a, b) (a) - (b) < -eps 
