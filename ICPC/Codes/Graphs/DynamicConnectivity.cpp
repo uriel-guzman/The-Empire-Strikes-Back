1774109e9589c4769e74a01cf115e5261f467acf
Abraham Murillo <abraham140600@gmail.com>
2020-10-16 12:18:34 -0500

    Dynamic connectivity update using minmax

--- ICPC/Codes/Graphs/DynamicConnectivity.cpp
+++ ICPC/Codes/Graphs/DynamicConnectivity.cpp
@@ -1,28 +1,25 @@
 struct DynamicConnectivity {
   struct Query {
-    char op;
-    int u, v, out;
+    int op, u, v, at;
   };
 
   Dsu dsu; // dsu with rollback
   vector<Query> queries;
-  vector< map<int, int> > mp;
+  map<ii, int> mp;
   int timer = -1;
  
-  DynamicConnectivity(int n = 0) : dsu(n), mp(n) {}
+  DynamicConnectivity(int n = 0) : dsu(n) {}
  
   void add(int u, int v) {
-    if (u > v) swap(u, v);
-    mp[u][v] = ++timer;
+    mp[minmax(u, v)] = ++timer;
     queries.pb({'+', u, v, INT_MAX});
   }
 
   void rem(int u, int v) {
-    if (u > v) swap(u, v);
-    int in = mp[u][v];
+    int in = mp[minmax(u, v)];
     queries.pb({'-', u, v, in});
-    queries[in].out = ++timer;
-    mp[u].erase(v);
+    queries[in].at = ++timer;
+    mp.erase(minmax(u, v));
   }
 
   void query() {
@@ -38,7 +35,7 @@ struct DynamicConnectivity {
     int before = sz(dsu.mem);
     for (int i = m + 1; i <= r; i++) {
       Query &q = queries[i];
-      if (q.out < l) 
+      if (q.op == '-' && q.at < l) 
         dsu.unite(q.u, q.v);
     }
     solve(l, m);
@@ -46,7 +43,7 @@ struct DynamicConnectivity {
       dsu.rollback();
     for (int i = l; i <= m; i++) {
       Query &q = queries[i];
-      if (q.out > r)
+      if (q.op == '+' && q.at > r)
         dsu.unite(q.u, q.v);
     }
     solve(m + 1, r);
