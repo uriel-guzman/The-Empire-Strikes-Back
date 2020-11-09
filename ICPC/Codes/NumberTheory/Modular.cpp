b5aabbea1a75154e0c49ef5a6062ea44653e980e
Abraham Murillo <abraham140600@gmail.com>
2020-11-09 13:05:51 -0600

    -1 line in Modular Int :D

--- ICPC/Codes/NumberTheory/Modular.cpp
+++ ICPC/Codes/NumberTheory/Modular.cpp
@@ -1,11 +1,5 @@
 template <int mod>
 struct Modular {
-  int v;
-  Modular(lli x = 0) : v(-mod < x && x < mod ? x : x % mod) {
-    if (v < 0) v += mod;
-    if (v >= mod) v -= mod;
-  }
-
   Modular operator + (const Modular &m) const { return Modular(v + m.v); }
   Modular operator - (const Modular &m) const { return Modular(v - m.v + mod); }
   Modular operator * (const Modular &m) const { return Modular(1LL * v * m.v); }
@@ -19,6 +13,11 @@ struct Modular {
   friend ostream & operator << (ostream &os, const Modular &m) { return os << m.v; }
   friend istream & operator >> (istream &is, Modular &m) { return is >> m.v; }
   
+  int v;
+  Modular(lli x = 0) : v(-mod < x && x < mod ? x : x % mod) {
+    if (v < 0) v += mod;
+    if (v >= mod) v -= mod;
+  }
 private:
   static int minv(int a, int m) {
     a %= m;
