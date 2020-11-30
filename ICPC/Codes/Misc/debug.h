f621b6d0fb78395367d928a1107d3807e1665b6d
Abraham Murillo <abraham140600@gmail.com>
2020-11-30 11:00:12 -0600

    readable debug

--- ICPC/Codes/Misc/debug.h
+++ ICPC/Codes/Misc/debug.h
@@ -18,14 +18,14 @@ void print(string s) { cout << endl; }
 
 template <class H, class... T>
 void print(string s, const H &h, const T&... t) {
-  const static string reset = "\033[0m";
+  const static string reset = "\033[0m", blue = "\033[1;34m", purple = "\033[3;95m";
   bool ok = 1;
   do { 
     if (s[0] == '\"') ok = 0;
-    else cout << "\033[1;34m" << s[0] << reset;
+    else cout << blue << s[0] << reset;
     s = s.substr(1);
   } while (s.size() && s[0] != ',');
-  if (ok) cout << ": " << "\033[3;95m" << h << reset;
+  if (ok) cout << ": " << purple << h << reset;
   print(s, t...);
 }
 
