dd7ef23b1ce0d0aa49eb1061d4aee815e82423a8
abrahamfourteen <36650502+abrahamfourteen@users.noreply.github.com>
2021-01-05 16:35:21 -0600

    Point renamed from P to Pt

--- ICPC/Codes/Geometry/Line.cpp
+++ ICPC/Codes/Geometry/Line.cpp
@@ -1,9 +1,9 @@
 struct Line {
-  P a, v;
+  Pt a, v;
 
-  Line(P a, P b) : a(a), v((b - a).unit()) {}
+  Line(Pt a, Pt b) : a(a), v((b - a).unit()) {}
 
-  bool contains(P p) {
+  bool contains(Pt p) {
     // pointInLine(a, v, p)
     return eq((p - a).cross(v), 0);  
   }
@@ -12,25 +12,24 @@ struct Line {
     // intersectLinesInfo(a1, v1, a2, v2)
     if (eq(v.cross(l.v), 0))
       return eq((l.a - a).cross(v), 0) ? -1 : 0;
-    else 
-      return 1;
+    return 1;
   }
 
   bool parallel(Line l) {
     return eq(v.cross(l.v), 0);
   }
 
-  P intersection(Line l) { 
+  Pt intersection(Line l) { 
     // intersectLines(a1, v1, a2, v2)
     // always check intersects() first!
     return a + v * ((l.a - a).cross(l.v) / v.cross(l.v));
   }
 
-  P projection(P p) {
+  Pt projection(Pt p) {
     return a + v * proj(p - a, v);
   }
 
-  P reflection(P p) {
+  Pt reflection(Pt p) {
     return 2 * a - p + v * 2 * proj(p - a, v);
   }
 };
