c746ab4830423d0fe3d369030528ac2e10618513
Abraham Murillo <abraham140600@gmail.com>
2021-01-07 09:20:22 -0600

    Removed const to be able to sort it

--- ICPC/Codes/Geometry/ClosestPairOfPoints.cpp
+++ ICPC/Codes/Geometry/ClosestPairOfPoints.cpp
@@ -1,12 +1,12 @@
-pair<Pt, Pt> closestPairOfPoints(const Poly &pts) {
+pair<Pt, Pt> closestPairOfPoints(Poly &pts) {
   sort(all(pts), [&](Pt a, Pt b) { 
     return le(a.y, b.y); 
   });
   set<Pt> st;
   ld ans = inf;
   Pt p, q;
-  int pos = 0, n = sz(pts);
-  fore(i, 0, n) {
+  int pos = 0;
+  fore (i, 0, sz(pts)) {
     while (pos < i && geq(pts[i].y - pts[pos].y, ans))
       st.erase(pts[pos++]);
     auto lo = st.lower_bound(Pt{pts[i].x - ans - eps, -inf});
