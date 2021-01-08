2889c6732f054d23dfc4afd0a41a68053a6d11d6
Abraham Murillo <abraham140600@gmail.com>
2021-01-07 20:50:04 -0600

    Geometry update

--- ICPC/Codes/Geometry/Segment.cpp
+++ ICPC/Codes/Geometry/Segment.cpp
@@ -4,12 +4,10 @@ struct Seg {
   Seg(Pt a, Pt b) : a(a), b(b), v(b - a) {}
   
   bool contains(Pt p) {
-    // pointInSegment(a, b, p)
     return eq(v.cross(p - a), 0) && leq((a - p).dot(b - p), 0);
   }
 
   int intersects(Seg s) {
-    // intersectSegmentsInfo(a, b, c, d) 
     int t1 = sgn(v.cross(s.a - a)), t2 = sgn(v.cross(s.b - a));
     if (t1 == t2) 
       return t1 == 0 && (contains(s.a) || contains(s.b) || s.contains(a) || s.contains(b)) ? -1 : 0; 
@@ -17,7 +15,6 @@ struct Seg {
   }
 
   Pt intersection(Seg s) { 
-    // Line.intersection(l), with a twist
     return a + v * ((s.a - a).cross(s.v) / v.cross(s.v));
   }
 };
