d5fccbfd54ea5076003557854034287140e05224
Abraham Murillo <abraham140600@gmail.com>
2021-01-25 19:18:51 -0600

    Shorter virtual tree

--- ICPC/Codes/Graphs/VirtualTree.cpp
+++ ICPC/Codes/Graphs/VirtualTree.cpp
@@ -4,29 +4,14 @@ int virtualTree(vi &ver) {
   auto byDfs = [&](int u, int v) {
     return tin[u] < tin[v];
   };
-  auto above = [&](int u, int v) {
-    return tin[u] <= tin[v] && tout[v] <= tout[u];
-  };
   sort(all(ver), byDfs);
-  int k = sz(ver);
-  fore (i, 1, k)
+  fore (i, sz(ver), 1)
     ver.pb(lca(ver[i - 1], ver[i]));
   sort(all(ver), byDfs);
   ver.erase(unique(all(ver)), ver.end());
   for (int u : ver)
     virt[u].clear();
-  vi stk = {ver[0]};
-  fore (i, 1, sz(ver)) {
-    int u = ver[i];
-    while (sz(stk) >= 2 && !above(stk.back(), u)) {
-      virt[stk.end()[-2]].pb(stk.back());
-      stk.pop_back();
-    }
-    stk.pb(u);
-  }
-  while (sz(stk) >= 2) {
-    virt[stk.end()[-2]].pb(stk.back());
-    stk.pop_back();
-  }
-  return stk[0];
+  fore (i, 1, sz(ver)) 
+    virt[lca(ver[i - 1], ver[i])].pb(ver[i]);
+  return ver[0];
 }
