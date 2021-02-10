959c657544bd8246733be1f228cb9e328fa39419
burinson <garentei@gmail.com>
2021-02-10 17:55:50 -0600

    Shorter D&C

--- ICPC/Codes/DynamicProgramming/DivideAndConquer.cpp
+++ ICPC/Codes/DynamicProgramming/DivideAndConquer.cpp
@@ -3,15 +3,11 @@ void dc(int cut, int l, int r, int optl, int optr) {
     return;
   int mid = (l + r) / 2;
   pair<lli, int> best = {inf, -1};
-  fore (p, optl, min(mid, optr) + 1) {
-    lli nxt = dp[~cut & 1][p - 1] + cost(p, mid);
-    if (nxt < best.f)
-      best = {nxt, p};
-  }
+  fore (p, optl, min(mid, optr) + 1) 
+    best = min(best, {dp[~cut & 1][p - 1] + cost(p, mid), p});
   dp[cut & 1][mid] = best.f;
-  int opt = best.s;
-  dc(cut, l, mid - 1, optl, opt);
-  dc(cut, mid + 1, r, opt, optr);
+  dc(cut, l, mid - 1, optl, best.s);
+  dc(cut, mid + 1, r, best.s, optr);
 } 
 
 fore (i, 1, n + 1)
