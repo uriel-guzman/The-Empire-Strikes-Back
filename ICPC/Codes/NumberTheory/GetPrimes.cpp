b65f0af77a3b025501519f78b3a44b5d012e74f0
burinson <garentei@gmail.com>
2021-02-26 18:14:46 -0600

    a

--- ICPC/Codes/NumberTheory/GetPrimes.cpp
+++ ICPC/Codes/NumberTheory/GetPrimes.cpp
@@ -1,14 +1,16 @@
 bitset<P> prime;
 vi primes;
 
-prime.set();
-prime[0] = prime[1] = 0;
+void getPrimes() {
+  prime.set();
+  prime[0] = prime[1] = 0;
 
-for (int i = 2; i * i < P; ++i)
-  if (prime[i])
-    for (int j = i * i; j < P; j += i)
-      prime[j] = 0;
+  for (int i = 2; i * i < P; ++i)
+    if (prime[i])
+      for (int j = i * i; j < P; j += i)
+        prime[j] = 0;
 
-fore (i, 2, P)
-  if (prime[i])
-    primes.pb(i);
+  fore (i, 2, P)
+    if (prime[i])
+      primes.pb(i);
+}
