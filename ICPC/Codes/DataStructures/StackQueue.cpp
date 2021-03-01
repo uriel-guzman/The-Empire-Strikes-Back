700d34ada8eaca2a112e1bb2e26038972cc80b8e
burinson <garentei@gmail.com>
2021-02-28 21:14:24 -0600

    wtf

--- ICPC/Codes/DataStructures/StackQueue.cpp
+++ ICPC/Codes/DataStructures/StackQueue.cpp
@@ -1,53 +1,48 @@
 template <class T, class F = function<T(const T&, const T&)>>
-struct Stack {
-  vector<T> s, t;
+struct Stack : vector<T> {
+  vector<T> s;
   F f;
   
   Stack(const F &f) : f(f) {}
-
+ 
   void push(T x) {
-    s.pb(x);
-    t.pb(t.empty() ? x : f(t.back(), x));
+    this->pb(x);
+    s.pb(s.empty() ? x : f(s.back(), x));
   }
-
+ 
   T pop() {
-    T x = s.back();
+    T x = this->back();
+    this->pop_back();
     s.pop_back();
-    t.pop_back();
     return x;
   }
-
-  bool empty() {
-    return s.empty();
-  }
-
+ 
   T query() {
-    return t.back();
+    return s.back();
   }
 };
-
+ 
 template <class T, class F = function<T(const T&, const T&)>>
 struct Queue {
   Stack<T> a, b;
   F f;
-
+ 
   Queue(const F &f) : a(f), b(f), f(f) {}
-
+ 
   void push(T x) {
     b.push(x);
   }
-
+ 
   void pop() {
     if (a.empty())
       while (!b.empty())
         a.push(b.pop());
     a.pop();
   }
-
+ 
   T query() {
     if (a.empty()) return b.query();
     if (b.empty()) return a.query();
     return f(a.query(), b.query());
   }
 };
-
