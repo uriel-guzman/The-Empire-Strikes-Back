01706206d035b01b1fd35141993cdae228ed44f2
Abraham Murillo <abraham140600@gmail.com>
2021-03-09 17:40:45 -0600

    Min rotation

---
+++ ICPC/Codes/Strings/Min rotation.cpp
@@ -0,0 +1,11 @@
+template <class T>
+int minRotation(vector<T> &s) {
+  int n = sz(s), i = 0, j = 1;
+  while (i < n && j < n) {
+    int k = 0;
+    while (k < n && s[(i + k) % n] == s[(j + k) % n]) k++;
+    (s[(i + k) % n] <= s[(j + k) % n] ? j : i) += k + 1;
+    j += i == j; 
+  }
+  return i < n ? i : j; 
+}
