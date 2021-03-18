95350187474ba28da9987b97a270c8f6f4af4792
Abraham Murillo <abraham140600@gmail.com>
2021-03-18 11:52:15 -0600

    Ordered tree can't work as multiset :(

--- ICPC/Codes/DataStructures/Ordered tree.cpp
+++ ICPC/Codes/DataStructures/Ordered tree.cpp
@@ -4,6 +4,5 @@ using namespace __gnu_pbds;
 
 template <class K, class V = null_type>
 using ordered_tree = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
-// less_equal<K> for multiset, multimap (?
 #define rank order_of_key
 #define kth find_by_order
