563493d1876fec1bee34a6e88516790cd6471734
Abraham Murillo <abraham140600@gmail.com>
2021-03-30 19:23:13 -0600

    Fraction fixed

--- ICPC/Codes/Math/Fraction.cpp
+++ ICPC/Codes/Math/Fraction.cpp
@@ -1,23 +1,54 @@
 struct Frac {
+  bool operator < (const Frac &f) { 
+    return n * f.d < f.n * d; 
+  }
+
+  bool operator > (const Frac &f) { 
+    return n * f.d > f.n * d; 
+  }
+
+  bool operator == (const Frac &f) { 
+    return n == f.n && d == f.d; 
+  }
+
+  bool operator != (const Frac &f) { 
+    return n != f.n || d != f.d; 
+  }
+
+  Frac operator - () const { 
+    return Frac(-n, d); 
+  }
+
+  Frac operator + (const Frac &f) { 
+    lli k = lcm(d, f.d);
+    return Frac(n * (k / d) + f.n * (k / f.d), k); 
+  }
+
+  Frac operator - (const Frac &f) { 
+    lli k = lcm(d, f.d);
+    return Frac(n * (k / d) - f.n * (k / f.d), k); 
+  }
+
+  Frac operator * (const Frac &f) { 
+    return Frac(n * f.n, d * f.d); 
+  }
+  
+  Frac operator / (const Frac &f) { 
+    return Frac(n * f.d, d * f.n); 
+  }
+
+  friend Frac abs(const Frac &f) { 
+    return Frac(abs(f.n), f.d); 
+  }
+  
+  friend ostream & operator << (ostream &os, const Frac &f) {
+    return os << f.n << "/" << f.d;
+  }
+
   lli n, d;
   Frac(lli a = 0, lli b = 1) {
     lli g = __gcd(a, b);
     n = a / g, d = b / g;
     if (d < 0) n *= -1, d *= -1;
-  }
-
-  bool operator < (const Frac &f) { return n * f.d < f.n * d; }
-  bool operator == (const Frac &f) { return n == f.n && d == f.d; }
-  bool operator != (const Frac &f) { return n != f.n || d != f.d; }
-
-  Frac operator - () const { return Frac(-n, d); }
-  Frac operator + (const Frac &f) { return Frac(n * f.d + d * f.n, d * f.d); }
-  Frac operator - (const Frac &f) { return Frac(n * f.d - d * f.n, d * f.d); }
-  Frac operator * (const Frac &f) { return Frac(n * f.n, d * f.d); }
-  Frac operator / (const Frac &f) { return Frac(n * f.d, d * f.n); }
-  friend Frac abs(const Frac &f) { return Frac(abs(f.n), f.d); }
-  
-  friend ostream & operator << (ostream &os, const Frac &f) {
-    return os << f.n << " / " << f.d;
   }
 };
