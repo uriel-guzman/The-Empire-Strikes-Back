d7d88937bf2074da4d65289f1b2f8ffae4cabce1
Abraham Baltazar Murillo Sandoval <36650502+abrahamfourteen@users.noreply.github.com>
2021-04-26 09:11:03 -0500

    Update Modular.cpp

--- ICPC/Codes/Math/Modular.cpp
+++ ICPC/Codes/Math/Modular.cpp
@@ -1,20 +1,28 @@
 template <int p>
 struct Modular {
+  int v;
+  
+  Modular(lli x = 0) {
+    if (abs(x) >= p) x %= p;
+    if (x < 0) x += p;
+    v = x;
+  }
+  
   Modular operator + (const Modular &m) const { return Modular(v + m.v); }
   Modular operator - (const Modular &m) const { return Modular(v - m.v + p); }
   Modular operator * (const Modular &m) const { return Modular(1LL * v * m.v); }
+  Modular operator / (const Modular &m) const { return *this * m.inv(); }
+  
   Modular inv() const { return Modular(minv(v, p)); } 
   Modular operator - () const { return Modular(-v); }
   
   Modular& operator += (const Modular &m) { return *this = *this + m; }
   Modular& operator -= (const Modular &m) { return *this = *this - m; }
   Modular& operator *= (const Modular &m) { return *this = *this * m; }
+  Modular& operator /= (const Modular &m) { return *this = *this / m; }
   
   friend ostream & operator << (ostream &os, const Modular &m) { return os << m.v; }
   friend istream & operator >> (istream &is, Modular &m) { return is >> m.v; }
-  
-  int v;
-  Modular(lli x = 0) : v((x %= p) < 0 ? x + p : x) {}
 private:
   static int minv(int a, int m) {
     a %= m;
@@ -22,5 +30,3 @@ private:
     return a == 1 ? 1 : int(m - lli(minv(m, a)) * lli(m) / a);
   }
 };
- 
-using Mint = Modular<int(1e9) + 7>;
