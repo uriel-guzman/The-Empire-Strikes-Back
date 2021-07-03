15163ef2fc275c1b4df0c63407c698a8ef560731
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2021-07-03 17:12:08 -0500

    Suffix array with sparse table for lcp and function to check s[a.f ... a.s] < s[b.f ... b.s]

--- ICPC/Codes/Strings/Suffix array.cpp
+++ ICPC/Codes/Strings/Suffix array.cpp
@@ -2,25 +2,37 @@ template <class T>
 struct SuffixArray {
   int n;
   T s;
-  vector<int> sa, rk, lcp;
-  
-  SuffixArray(const T &a) : n(sz(a) + 1), s(a), sa(n), rk(n), lcp(n) {
+  vector<int> sa, rank;
+  vector<vi> sp;
+
+  SuffixArray(const T &x) : n(sz(x) + 1), s(x), sa(n), rank(n), sp(1 + __lg(n), vi(n, 0)) {
     s.pb(0);
-    fore (i, 0, n) sa[i] = i, rk[i] = s[i];
-    vector<int> nsa(n), nrk(n), cnt(max(260, n));
-    for (int k = 0; k < n; k ? k *= 2 : k++) {
+    fore (i, 0, n) sa[i] = i, rank[i] = s[i];
+    vector<int> nsa(n), cnt(max(260, n));
+    for (int k = 0; k < n && rank[sa[n - 1]] != n - 1; k ? k *= 2 : k++) {
       fill(all(cnt), 0);
-      fore (i, 0, n) nsa[i] = (sa[i] - k + n) % n, cnt[rk[i]]++;
+      fore (i, 0, n) nsa[i] = (sa[i] - k + n) % n, cnt[rank[i]]++;
       partial_sum(all(cnt), cnt.begin());
-      fore (i, n, 0) sa[--cnt[rk[nsa[i]]]] = nsa[i];
-      for (int i = 1, r = 0; i < n; i++) 
-        nrk[sa[i]] = r += rk[sa[i]] != rk[sa[i - 1]] || rk[(sa[i] + k) % n] != rk[(sa[i - 1] + k) % n];;
-      rk.swap(nrk);
-      if (rk[sa[n - 1]] == n - 1) break;
+      fore (i, n, 0) sa[--cnt[rank[nsa[i]]]] = nsa[i];
+      vector<int> prev(rank);
+      for (int i = 1, r = 0; i < n; i++) {
+        if (prev[sa[i]] != prev[sa[i - 1]] || prev[(sa[i] + k) % n] != prev[(sa[i - 1] + k) % n]) r++;
+        rank[sa[i]] = r;
+      }
     }
-    for (int i = 0, j = rk[lcp[0] = 0], k = 0; i < n - 1; i++, k++) 
+    for (int i = 0, j = rank[0], k = 0; i < n - 1; i++, k++) 
       while (k >= 0 && s[i] != s[sa[j - 1] + k])
-        lcp[j] = k--, j = rk[sa[j] + 1];  
+        sp[0][j] = k--, j = rank[sa[j] + 1];  
+    for (int k = 1; (1 << k) < n; k++) 
+      fore (l, 0, n - (1 << (k - 1))) {
+        int r = l + (1 << (k - 1)); 
+        sp[k][l] = min(sp[k - 1][l], sp[k - 1][r]);
+      }
+  }
+
+  int lcp(int l, int r) { 
+    int k = __lg(r - ++l + 1);
+    return min(sp[k][l], sp[k][r - (1 << k) + 1]);
   }
 
   auto at(int i, int j) {
@@ -40,5 +52,14 @@ struct SuffixArray {
       if (at(l, i) != t[i] && at(r, i) != t[i] || l > r) return 0;
     }
     return r - l + 1;
+  }
+
+  bool compare(ii a, ii b) {
+    // s[a.f ... a.s] < s[b.f ... b.s]
+    ii range = minmax(rank[a.f], rank[b.f]);
+    int common = lcp(range.f, range.s);
+    int szA = a.s - a.f + 1, szB = b.s - b.f + 1;
+    if (common >= min(szA, szB)) return szA < szB;
+    return rank[a.f] < rank[b.f];
   }
 };
