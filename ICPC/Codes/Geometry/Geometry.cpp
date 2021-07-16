d86808184f8671329e38f2a56f86eb154b269945
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2021-07-16 13:22:45 -0500

    Geometry.cpp updated

--- ICPC/Codes/Geometry/Geometry.cpp
+++ ICPC/Codes/Geometry/Geometry.cpp
@@ -1,9 +1,14 @@
+struct Cir;
+struct Pt;
+struct Line;
+struct Seg;
+
 const ld eps = 1e-20;
+const ld pi = acos(-1.0);
+enum {ON = -1, OUT, IN, OVERLAP, INF};
 #define eq(a, b) (abs((a) - (b)) <= +eps)
 #define neq(a, b) (!eq(a, b))
 #define geq(a, b) ((a) - (b) >= -eps)
 #define leq(a, b) ((a) - (b) <= +eps)
 #define ge(a, b) ((a) - (b) > +eps)
 #define le(a, b) ((a) - (b) < -eps) 
-
-enum {ON = -1, OUT, IN, OVERLAP, INF};
