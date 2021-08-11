0cd9d3b9ad6be19c07946ce0d13c70e8285baf84
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2021-08-11 16:32:06 -0500

    Modular is back

--- ICPC/Codes/Math/Modular.cpp
+++ ICPC/Codes/Math/Modular.cpp
@@ -1,16 +1,21 @@
 template <const int M>
 struct Modular {
   int v;
-  Modular(int v = 0) : v(v) {}
-  Modular(int64_t v) : v(v % M) {}
+  Modular(int a = 0) : v(a) {}
+  Modular(int64_t a) : v((a < 0 ? M - -a % M : a) % M) {}
     
   Modular operator + (Modular m) { return Modular((v + m.v) % M); };
   Modular operator - (Modular m) { return Modular((v - m.v + M) % M); };
   Modular operator * (Modular m) { return Modular((1LL * v * m.v) % M); };
   Modular operator / (Modular m) { return *this * m.inv(); };
   Modular inv() { return this->pow(M - 2); }
+
+  Modular& operator += (Modular m) { return *this = *this + m; }
+  Modular& operator -= (Modular m) { return *this = *this - m; }
+  Modular& operator *= (Modular m) { return *this = *this * m; }
+  Modular& operator /= (Modular m) { return *this = *this / m; }
  
-  friend ostream & operator << (ostream &os, const Modular &m) { return os << m.v; }
+  friend ostream & operator << (ostream &os, Modular m) { return os << m.v; }
   template <class T> bool operator == (T x) { return v == x; }
 
   Modular pow(lli n) {
