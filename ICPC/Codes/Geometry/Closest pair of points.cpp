e693a05b67e55905fc6b3cf508258f8a2a57d60e
Roberto Jesus Garcia Pino <pino@Robertos-MacBook-Air.local>
2021-11-09 15:03:32 -0600

    closetPairOfPints update

--- ICPC/Codes/Geometry/Closest pair of points.cpp
+++ ICPC/Codes/Geometry/Closest pair of points.cpp
@@ -1,16 +1,17 @@
 pair<Pt, Pt> closestPairOfPoints(vector<Pt> &pts) {
-  sort(all(pts), [&](Pt a, Pt b) { 
-    return le(a.y, b.y); 
+  sort(all(pts), [&](Pt a, Pt b) {
+    return le(a.x, b.x); 
   });
+  // set sorted by smaller y coordinate
   set<Pt> st;
   ld ans = INF;
   Pt p, q;
   int pos = 0;
   fore (i, 0, sz(pts)) {
-    while (pos < i && geq(pts[i].y - pts[pos].y, ans))
+    while (pos < i && geq(pts[i].x - pts[pos].x, ans))
       st.erase(pts[pos++]);
-    auto lo = st.lower_bound(Pt(pts[i].x - ans - eps, -INF));
-    auto hi = st.upper_bound(Pt(pts[i].x + ans + eps, -INF));
+    auto lo = st.lower_bound(Pt(pts[i].y - ans - eps, -INF));
+    auto hi = st.upper_bound(Pt(pts[i].y + ans + eps, -INF));
     for (auto it = lo; it != hi; ++it) {
       ld d = (pts[i] - *it).length();
       if (le(d, ans)) 
