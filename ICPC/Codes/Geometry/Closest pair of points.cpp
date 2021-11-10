69e85c1a5cae624f276694a10a4e48330fc8ba70
Roberto Jesus Garcia Pino <pino@Robertos-MacBook-Air.local>
2021-11-10 00:26:05 -0600

    Returning back closestPairOfPoints.cpp

--- ICPC/Codes/Geometry/Closest pair of points.cpp
+++ ICPC/Codes/Geometry/Closest pair of points.cpp
@@ -1,17 +1,17 @@
 pair<Pt, Pt> closestPairOfPoints(vector<Pt> &pts) {
-  sort(all(pts), [&](Pt a, Pt b) {
-    return le(a.x, b.x); 
+  sort(all(pts), [&](Pt a, Pt b) { 
+    return le(a.y, b.y); 
   });
-  // set sorted by smaller y coordinate
+  // set sorted by smallest x coordinate and then by smallest y coordinate
   set<Pt> st;
   ld ans = INF;
   Pt p, q;
   int pos = 0;
   fore (i, 0, sz(pts)) {
-    while (pos < i && geq(pts[i].x - pts[pos].x, ans))
+    while (pos < i && geq(pts[i].y - pts[pos].y, ans))
       st.erase(pts[pos++]);
-    auto lo = st.lower_bound(Pt(-INF, pts[i].y - ans - EPS));
-    auto hi = st.upper_bound(Pt(-INF, pts[i].y + ans + EPS));
+    auto lo = st.lower_bound(Pt(pts[i].x - ans - EPS, -INF));
+    auto hi = st.upper_bound(Pt(pts[i].x + ans + EPS, -INF));
     for (auto it = lo; it != hi; ++it) {
       ld d = (pts[i] - *it).length();
       if (le(d, ans)) 
