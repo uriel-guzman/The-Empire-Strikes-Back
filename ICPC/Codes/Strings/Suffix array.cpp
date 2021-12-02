6c8625138ce678f3fc0a6eca7cd0a0412da35e4d
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2021-12-02 11:58:39 -0600

    Suffix array fixed (?

--- ICPC/Codes/Strings/Suffix array.cpp
+++ ICPC/Codes/Strings/Suffix array.cpp
@@ -2,43 +2,46 @@ template <class T>
 struct SuffixArray {
   int n;
   T s;
-  vector<int> sa, rank, sp[25];
+  vector<int> sa, pos, dp[25];
 
-  SuffixArray(const T& x) : n(sz(x) + 1), s(x), sa(n), rank(n) {
+  SuffixArray(const T& x) : n(sz(x) + 1), s(x), sa(n), pos(n) {
     s.pb(0);
     fore (i, 0, n)
-      sa[i] = i, rank[i] = s[i];
-    vector<int> nsa(n), cnt(max(260, n));
-    for (int k = 0; k < n && rank[sa[n - 1]] != n - 1; k ? k *= 2 : k++) {
+      sa[i] = i, pos[i] = s[i];
+    vector<int> nsa(sa), npos(n), cnt(max(260, n), 0);
+    for (int k = 0; k < n; k ? k *= 2 : k++) {
       fill(all(cnt), 0);
       fore (i, 0, n)
-        nsa[i] = (sa[i] - k + n) % n, cnt[rank[i]]++;
+        nsa[i] = (sa[i] - k + n) % n, cnt[pos[i]]++;
       partial_sum(all(cnt), cnt.begin());
       fore (i, n, 0)
-        sa[--cnt[rank[nsa[i]]]] = nsa[i];
-      vector<int> prev(rank);
-      for (int i = 1, r = 0; i < n; i++) {
-        if (prev[sa[i]] != prev[sa[i - 1]] || prev[(sa[i] + k) % n] != prev[(sa[i - 1] + k) % n])
-          r++;
-        rank[sa[i]] = r;
+        sa[--cnt[pos[nsa[i]]]] = nsa[i];
+      for (int i = 1, cur = 0; i < n; i++) {
+        cur += (pos[sa[i]] != pos[sa[i - 1]] || pos[(sa[i] + k) % n] != pos[(sa[i - 1] + k) % n]);
+        npos[sa[i]] = cur;
       }
+      pos = npos;
+      if (pos[sa[n - 1]] >= n - 1)
+        break;
     }
-    sp[0].resize(n);
-    for (int i = 0, j = rank[0], k = 0; i < n - 1; i++, k++)
+    dp[0].assign(n, 0);
+    for (int i = 0, j = pos[0], k = 0; i < n; ++i, ++k) {
       while (k >= 0 && s[i] != s[sa[j - 1] + k])
-        sp[0][j] = k--, j = rank[sa[j] + 1];
-    for (int k = 1; (1 << k) < n; k++) {
-      sp[k].resize(n - (1 << k) + 1);
-      fore (l, 0, n - (1 << (k - 1))) {
-        int r = l + (1 << (k - 1));
-        sp[k][l] = min(sp[k - 1][l], sp[k - 1][r]);
-      }
+        dp[0][j] = k--, j = pos[sa[j] + 1];
+    }
+    for (int k = 1, pw = 1; pw < n; k++, pw <<= 1) {
+      dp[k].assign(n, 0);
+      for (int l = 0; l + pw < n; l++)
+        dp[k][l] = min(dp[k - 1][l], dp[k - 1][l + pw]);
     }
   }
 
   int lcp(int l, int r) {
-    int k = __lg(r - ++l + 1);
-    return min(sp[k][l], sp[k][r - (1 << k) + 1]);
+    if (l == r)
+      return n - l;
+    tie(l, r) = minmax(pos[l], pos[r]);
+    int k = __lg(r - l);
+    return min(dp[k][l + 1], dp[k][r - (1 << k) + 1]);
   }
 
   auto at(int i, int j) {
@@ -65,11 +68,10 @@ struct SuffixArray {
 
   bool compare(ii a, ii b) {
     // s[a.f ... a.s] < s[b.f ... b.s]
-    ii range = minmax(rank[a.f], rank[b.f]);
-    int common = lcp(range.f, range.s);
+    int common = lcp(a.f, b.f);
     int szA = a.s - a.f + 1, szB = b.s - b.f + 1;
     if (common >= min(szA, szB))
-      return szA < szB;
-    return rank[a.f] < rank[b.f];
+      return szA == szB ? a < b : szA < szB;
+    return s[a.f + common] < s[b.f + common];
   }
 };
