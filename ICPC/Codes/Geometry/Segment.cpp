29807b6942f3bedd03bc2c1f31fc3de71b8d42ef
burinson <garentei@gmail.com>
2021-12-10 14:14:12 -0600

    Edmond Karp's blossom algorithm

--- ICPC/Codes/Geometry/Segment.cpp
+++ ICPC/Codes/Geometry/Segment.cpp
@@ -11,9 +11,8 @@ struct Seg {
   int intersects(Seg s) {
     int t1 = sgn(v.cross(s.a - a)), t2 = sgn(v.cross(s.b - a));
     if (t1 == t2)
-      return t1 == 0 && (contains(s.a) || contains(s.b) || s.contains(a) || s.contains(b)) ? INF
-                                                                                           : 0;
-    return sgn(s.v.cross(a - s.a)) != sgn(s.v.cross(b - s.a));
+      return t1 == 0 && (contains(s.a) || contains(s.b) || s.contains(a) || s.contains(b))
+          ? INF return sgn(s.v.cross(a - s.a)) != sgn(s.v.cross(b - s.a));
   }
 
   template <class Seg>
