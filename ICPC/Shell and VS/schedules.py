e69bb665917a789a8787794a34a34b879ab31a16
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2021-12-13 07:59:49 -0600

    Schedules minor update

--- ICPC/Shell and VS/schedules.py
+++ ICPC/Shell and VS/schedules.py
@@ -18,7 +18,7 @@ def groupBySameKey(courses):
 
 def getCourses(year, coursesKeysSet, ignoreNRC):
   career = 'INCO'
-  url = 'http://consulta.siiau.udg.mx/wco/sspseca.consulta_oferta?ciclop={}&cup=D&majrp=INCO&crsep=&materiap=&horaip=&horafp=&edifp=&aulap=&ordenp=0&mostrarp=1000'.format(year)
+  url = f'http://consulta.siiau.udg.mx/wco/sspseca.consulta_oferta?ciclop={year}&cup=D&majrp=INCO&crsep=&materiap=&horaip=&horafp=&edifp=&aulap=&ordenp=0&mostrarp=1000'
   
   r = requests.get(url)
   soup = BeautifulSoup(r.text, 'html.parser')
@@ -49,17 +49,17 @@ def getCourses(year, coursesKeysSet, ignoreNRC):
 
       days = []
       for i in range(h + 1):
-        start = data[i+11][2:6]
-        end = data[i+11][7:11]
+        start = data[i + 11][2:6]
+        end = data[i + 11][7:11]
         
         whichDays = []
-        for day in data[i+11][11:22]:
+        for day in data[i + 11][11:22]:
           if day.isalpha():
             whichDays.append(day)
             
         # Alpha y Beta
         isDuct = 0
-        if data[i+11][22:26] == 'DUCT':
+        if data[i + 11][22:26] == 'DUCT':
           isDuct = 1
 
         building = data[i + 11][22 : 26 + isDuct]
@@ -67,8 +67,8 @@ def getCourses(year, coursesKeysSet, ignoreNRC):
         
         for day in whichDays:
           days.append({
-            'start': start,
-            'end': end,
+            'start': int(start),
+            'end': int(end),
             'day': day,
             'building': building,
             'classroom': classroom
@@ -119,7 +119,7 @@ def generateSchedules(coursesKeys, courses, preferred):
       def valid(days): 
         # Check if we can append this days to the 'current' schedule
         for day in days:
-          if int(preferred['start']) <= int(day['start']) and int(day['end']) <= int(preferred['end']):
+          if preferred['start'] <= day['start'] and day['end'] <= preferred['end']:
             if day['day'] not in preferred['days']:
               return False      
             if getKey(day) in currentSchedule.keys():
@@ -161,16 +161,14 @@ def prettySchedule(schedule):
         break
       weekDay += 1
 
-    start = int(key['start']) // 100
-    end = int(key['end']) // 100
+    start = key['start'] // 100
+    end = key['end'] // 100
 
     for i in range(start, end + 1):
       day[i - 6][weekDay] = data['nrc']
   
   print(tabulate(day, tablefmt='fancy_grid'))
-
-  df = pd.DataFrame(info)
-  print(tabulate(df.T, headers="keys"))
+  print(tabulate(pd.DataFrame(info).T, headers="keys"))
 
 if __name__ == '__main__':
   # Year to select
@@ -181,15 +179,16 @@ if __name__ == '__main__':
   ignoreNRC = {"153405", "164138"}
   # Preferred time range and days
   preferred = {
-    'start': '0700',
-    'end': '2000',
+    'start': 700,
+    'end': 2000,
     'days': "LMIJVS",
   }
   
   courses = getCourses(year, coursesKeysSet, ignoreNRC)
   schedules = generateSchedules(list(coursesKeysSet), courses, preferred)
   
-  for i in schedules:
-    prettySchedule(i)
+  for pos, schedule in enumerate(schedules):
+    print(f"{pos + 1} / {len(schedules)}")
+    prettySchedule(schedule)
     print()
   
