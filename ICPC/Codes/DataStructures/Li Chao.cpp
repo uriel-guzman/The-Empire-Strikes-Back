4af4d50c5aeaba863a7fc29c582bd5474ba657ef
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2021-12-16 12:52:12 -0600

    Li chao cleaner

--- ICPC/Codes/DataStructures/Li Chao.cpp
+++ ICPC/Codes/DataStructures/Li Chao.cpp
@@ -1,28 +1,23 @@
-struct Fun {
-  lli m = 0, c = INF;
-  lli operator()(lli x) const {
-    return m * x + c;
-  }
-};
-
 struct LiChao {
+  struct Fun {
+    lli m = 0, c = -INF;
+    lli operator()(lli x) const {
+      return m * x + c;
+    }
+  } f;
+
   lli l, r;
   LiChao *left, *right;
-  Fun f;
-
-  LiChao(lli l, lli r, Fun f) : l(l), r(r), f(f) left(0), right(0) {}
+  LiChao(lli l, lli r, Fun f) : l(l), r(r), f(f), left(0), right(0) {}
 
   void add(Fun& g) {
-    if (f(l) <= g(l) && f(r) <= g(r))
-      return;
-    if (g(l) < f(l) && g(r) < f(r)) {
-      f = g;
-      return;
-    }
     lli m = (l + r) >> 1;
-    if (g(m) < f(m))
+    bool bl = g(l) > f(l), bm = g(m) > f(m);
+    if (bm)
       swap(f, g);
-    if (g(l) <= f(l))
+    if (l == r)
+      return;
+    if (bl != bm)
       left = left ? (left->add(g), left) : new LiChao(l, m, g);
     else
       right = right ? (right->add(g), right) : new LiChao(m + 1, r, g);
@@ -33,7 +28,7 @@ struct LiChao {
       return f(x);
     lli m = (l + r) >> 1;
     if (x <= m)
-      return min(f(x), left ? left->query(x) : INF);
-    return min(f(x), right ? right->query(x) : INF);
+      return max(f(x), left ? left->query(x) : -INF);
+    return max(f(x), right ? right->query(x) : -INF);
   }
 };
