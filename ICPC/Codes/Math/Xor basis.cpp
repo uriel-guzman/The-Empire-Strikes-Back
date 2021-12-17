77a759eefa6c592bc184f59a8e64c637f39f72d5
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2021-12-17 14:44:01 -0600

    Xor basis recover elements

--- ICPC/Codes/Math/Xor basis.cpp
+++ ICPC/Codes/Math/Xor basis.cpp
@@ -1,52 +1,71 @@
 template <int D>
 struct XorBasis {
-  array<int, D> basis;
-  int n = 0;
+  using Num = bitset<D>;
+  array<Num, D> basis, keep;
+  vector<int> from;
+  int n = 0, id = -1;
 
-  XorBasis() {
+  XorBasis() : from(D, -1) {
     basis.fill(0);
   }
 
-  bool insert(int x) {
+  bool insert(Num x) {
+    ++id;
+    Num k;
     fore (i, D, 0)
-      if ((x >> i) & 1) {
-        if (!basis[i]) {
+      if (x[i]) {
+        if (!basis[i].any()) {
+          k[i] = 1, from[i] = id, keep[i] = k;
           basis[i] = x, n++;
           return 1;
         }
-        x ^= basis[i];
+        x ^= basis[i], k ^= keep[i];
       }
     return 0;
   }
 
-  optional<int> find(int x) {
-    // which number is needed to generate x
-    // num ^ (num ^ x) = x
-    int num = 0;
+  optional<Num> find(Num x) {
+    // is x in xor-basis set?
+    // v ^ (v ^ x) = x
+    Num v;
     fore (i, D, 0)
-      if ((x >> i) & 1) {
-        if (!basis[i])
+      if (x[i]) {
+        if (!basis[i].any())
           return nullopt;
         x ^= basis[i];
-        num |= (1 << i);
+        v[i] = 1;
       }
-    return optional(num);
+    return optional(v);
   }
 
-  optional<int> operator[](lli k) {
+  optional<vector<int>> recover(Num x) {
+    auto v = find(x);
+    if (!v)
+      return nullopt;
+    Num tmp;
+    fore (i, D, 0)
+      if (v.value()[i])
+        tmp ^= keep[i];
+    vector<int> ans;
+    for (int i = tmp._Find_first(); i < D; i = tmp._Find_next(i))
+      ans.pb(from[i]);
+    return ans;
+  }
+
+  optional<Num> operator[](lli k) {
     lli tot = (1LL << n);
     if (k > tot)
       return nullopt;
-    int num = 0;
+    Num v = 0;
     fore (i, D, 0)
       if (basis[i]) {
         lli low = tot / 2;
-        if ((low < k && ((num >> i) & 1) == 0) || (low >= k && ((num >> i) & 1)))
-          num ^= basis[i];
+        if ((low < k && v[i] == 0) || (low >= k && v[i]))
+          v ^= basis[i];
         if (low < k)
           k -= low;
         tot /= 2;
       }
-    return optional(num);
+    return optional(v);
   }
 };
