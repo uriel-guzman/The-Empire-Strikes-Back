156b78a90a6d94136ee9e31dd5eac1fbe520914b
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2022-01-01 16:49:23 -0600

    Lazy segtree without template, it's easier to modify this way

--- ICPC/Codes/DataStructures/Lazy segtree.cpp
+++ ICPC/Codes/DataStructures/Lazy segtree.cpp
@@ -1,80 +1,54 @@
-template <class Node>
 struct Lazy {
   int l, r;
   Lazy *left, *right;
-  Node node;
+  lli sum = 0, lazy = 0;
 
-  template <class Arr>
-  Lazy(int l, int r, Arr& a) : l(l), r(r), left(0), right(0) {
+  Lazy(int l, int r) : l(l), r(r), left(0), right(0) {
     if (l == r) {
-      node.val = {a[l]};
+      sum = a[l];
       return;
     }
     int m = (l + r) >> 1;
-    left = new Lazy(l, m, a);
-    right = new Lazy(m + 1, r, a);
+    left = new Lazy(l, m);
+    right = new Lazy(m + 1, r);
     pull();
   }
 
   void push() {
-    if (node.lazy()) {
-      int m = (l + r) >> 1;
-      left->node.apply(l, m, node.lazy);
-      right->node.apply(m + 1, r, node.lazy);
-      node.lazy = {};
+    if (!lazy)
+      return;
+    sum += (r - l + 1) * lazy;
+    if (l != r) {
+      left->lazy += lazy;
+      right->lazy += lazy;
     }
+    lazy = 0;
   }
 
   void pull() {
-    node.val = left->node.val + right->node.val;
+    sum = left->sum + right->sum;
   }
 
-  template <class... Args>
-  void update(int ll, int rr, const Args&... args) {
+  void update(int ll, int rr, lli v) {
+    push();
     if (rr < l || r < ll)
       return;
     if (ll <= l && r <= rr) {
-      node.apply(l, r, typename Node::Lazy(args...));
+      lazy += v;
+      push();
       return;
     }
-    push();
-    left->update(ll, rr, args...);
-    right->update(ll, rr, args...);
+    left->update(ll, rr, v);
+    right->update(ll, rr, v);
     pull();
   }
 
-  auto query(int ll, int rr) {
-    if (rr < l || r < ll)
-      return typename Node::Value();
-    if (ll <= l && r <= rr)
-      return node.val;
+  lli query(int ll, int rr) {
     push();
+    if (rr < l || r < ll)
+      return 0;
+    if (ll <= l && r <= rr)
+      return sum;
     return left->query(ll, rr) + right->query(ll, rr);
-  }
-};
-
-struct Node {
-  struct Lazy {
-    int add;
-    Lazy(int x = 0) : add(x) {}
-
-    inline bool operator()() const {
-      return add != 0;
-    }
-  } lazy;
-
-  struct Value {
-    int sum;
-    Value(int x = 0) : sum(x) {}
-
-    friend Value operator+(Value l, const Value& r) {
-      l.sum += r.sum;
-      return l;
-    }
-  } val;
-
-  inline void apply(int l, int r, const Lazy& tag) {
-    val.sum += tag.add;
-    lazy.add += tag.add;
   }
 };
