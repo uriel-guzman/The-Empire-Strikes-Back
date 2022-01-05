5a8dcd5930e6131290201d4d3c5f62a2b7a4dc80
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2022-01-05 11:21:59 -0600

    Book update with some .tex

--- ICPC/Codes/Dynamic programming/Knapsack 01.cpp
+++ ICPC/Codes/Dynamic programming/Knapsack 01.cpp
@@ -1,7 +1,3 @@
-struct Item {
-  int w, cost;
-};
-
-for (auto& cur : items)
-  for (int w = W; w >= cur.w; w--)
-    umax(dp[w], dp[w - cur.w] + cur.cost);
+fore (i, 0, n)
+  for (int x = MaxW; x >= w[i]; x--)
+    umax(dp[x], dp[x - w[i]] + cost[i]);
