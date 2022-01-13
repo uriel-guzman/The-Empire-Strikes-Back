063e3d0c43f8717feeb90f21ca8890ba20bfbba5
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2022-01-13 12:49:08 -0600

    Shortcuts update

--- ICPC/Shell and VS/Abraham/shortcuts.sh
+++ ICPC/Shell and VS/Abraham/shortcuts.sh
@@ -1,13 +1,15 @@
-alias myBash="open ~/.zshenv"
-alias saveMyBash="source ~/.zshenv" 
-
 user="/Users/abraham"
 icpcDir="${user}/The-Empire-Strikes-Back/ICPC"
+
+alias myBash="open ~/.zshenv"
+alias saveMyBash="source ~/.zshenv" 
 
 alias icpc="cd ${icpcDir}" 
 alias problems="cd ${user}/Problems"
 alias snippets="python3 ${icpcDir}/Shell\ and\ VS/createSnippets.py"
 alias omegaup="python3 ${icpcDir}/Shell\ and\ VS/omegaup.py"
+
+templateCode="${icpcDir}/Codes/Misc/Template.cpp"
 
 red='\x1B[0;31m'
 green='\x1B[0;32m'
@@ -34,48 +36,45 @@ solve() {
 }
 
 create() {
-  templateCode="${icpcDir}/Codes/Misc/Template.cpp"
-
-  begin=$1
-  end=$1
   if [ $# -ge 2 ]; then
     # It's a contest
+    begin=$1
     end=$2
     tee {${begin}..${end}}.cpp < ${templateCode}
     touch {${begin}..${end}} 
   else
     # A single file
-    tee ${begin}.cpp < ${templateCode}
-    open ${begin}.cpp
+    tee $1.cpp < ${templateCode}
+    open $1.cpp
     touch in
   fi
 }
 
 erase() {
-  # Erase anything in between [begin, end]
-  begin=$1
-  end=$1
   if [ $# -ge 2 ]; then
+    # Erase anything in between [begin, end]
+    begin=$1
     end=$2 
-    rm -r {${begin}..${end}}.cpp 
-    rm -r {${begin}..${end}} 
+    rm {${begin}..${end}}.cpp
+    rm {${begin}..${end}}  
   else
-    file=${begin}
-    [ ! -e ${file}.cpp ] || rm -r ${file}.cpp
-    [ ! -e ${file} ] || rm -r ${file}
+    # Erase all with prefix $1 and any extension
+    pref="${1%.*}"
+    rm ${pref}.*
+    rm -r ${pref}
   fi
 }
 
 compile() {
-  # Compiles 'file' with g++ and using 'moreFlags'
-  file=$1
+  # Compiles 'filename' with g++ and using 'moreFlags'
+  filename=$1
   moreFlags=""
   if [ $# -ge 2 ]; then
     moreFlags=$2
   fi
 
-  alias flags='-Wall -Wextra -Wfatal-errors -Wshadow -w -mcmodel=medium'
-  g++-11 --std=c++17 ${moreFlags} ${flags} ${file}.cpp -o ${file} 
+  alias flags='-Wall -Wextra -Wfatal-errors -Wshadow -w -mcmodel=large'
+  g++-11 --std=c++17 ${moreFlags} ${flags} ${filename}.cpp -o ${filename}.out 
 }
 
 go() {
@@ -87,7 +86,7 @@ go() {
 
   compile ${filename} ${flags}
   
-  ./${filename} < ${input}
+  ./${filename}.out < ${input}
 }
 
 run() {
@@ -99,46 +98,50 @@ debug() {
 } 
 
 random() {
-  # random solution [bruteForceSolution] [testCaseGenerator]
+  # random solution [brute] [generator]
 
   solution=$1
-  bruteForceSolution="brute"
-  testCaseGenerator="gen"
+  brute="brute"
+  generator="gen"
   if [ $# -ge 2 ]; then
-    bruteForceSolution=$2
+    brute=$2
     if [ $# -ge 3 ]; then
-      testCaseGenerator=$3
+      generator=$3
     fi
   fi
 
   compile ${solution} 
-  compile ${bruteForceSolution}
+  compile ${brute}
 
-  if [[ -f ${testCaseGenerator}.cpp ]]; then
+  if [[ -f ${generator}.cpp ]]; then
     # C++ version, so first compile it
-    compile ${testCaseGenerator}
+    compile ${generator}
   fi
 
   generateTestCase() {
-    # cpp > py testCaseGenerator
-    if [[ -f ${testCaseGenerator}.cpp ]]; then
-      ./${testCaseGenerator} > in
+    # cpp > py generator
+    if [[ -f ${generator}.cpp ]]; then
+      ./${generator}.out > in
     else
-      python3 ${testCaseGenerator}.py | cat > in 
+      python3 ${generator}.py | cat > in 
     fi
   }
 
   for ((i = 1; i <= 300; i++)); do
-    generateTestCase
-
     printf "Test case #${i}"
 
-    diff -ywi <(./${solution} < in) <(./${bruteForceSolution} < in) > diff${solution}
+    generateTestCase
+
+    ./${solution}.out < in > myOut
+    ./${brute}.out < in > bruteForceOut
+
+    diff -ywi myOut bruteForceOut > diff${solution}
 
     if [[ $? -eq 0 ]]; then
       printf "${green} Accepted ${removeColor}\n"
     else
       printf "${red} Wrong answer ${removeColor}\n"
+      open diff${solution}
       break
     fi
   done
