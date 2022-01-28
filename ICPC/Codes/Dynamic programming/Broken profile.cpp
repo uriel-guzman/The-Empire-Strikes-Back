a1cdef7aaa06304e6926309ede4694c3ca8a78a3
burinson <garentei@gmail.com>
2022-01-28 10:22:25 -0600

    Swapped states in broken profile dp

--- ICPC/Codes/Dynamic programming/Broken profile.cpp
+++ ICPC/Codes/Dynamic programming/Broken profile.cpp
@@ -1,4 +1,4 @@
-lli dp[2][1 << N][N];
+lli dp[2][N][1 << N];
 
 dp[0][0][0] = 1;
 
@@ -6,24 +6,24 @@ fore (c, 0, m) {
   fore (r, 0, n + 1) {
     fore (mask, 0, 1 << n) {
       if (r == n) {
-        dp[~c & 1][mask][0] += dp[c & 1][mask][r];
+        dp[~c & 1][0][mask] += dp[c & 1][r][mask];
         continue;
       }
 
       if (~(mask >> r) & 1) {
-        dp[c & 1][mask | (1 << r)][r + 1] += dp[c & 1][mask][r];
+        dp[c & 1][r + 1][mask | (1 << r)] += dp[c & 1][r][mask];
 
         if (~(mask >> (r + 1)) & 1)
-          dp[c & 1][mask][r + 2] += dp[c & 1][mask][r];
+          dp[c & 1][r + 2][mask] += dp[c & 1][r][mask];
       } else {
-        dp[c & 1][mask & ~(1 << r)][r + 1] += dp[c & 1][mask][r];
+        dp[c & 1][r + 1][mask & ~(1 << r)] += dp[c & 1][r][mask];
       }
     }
   }
 
   fore (r, 0, n + 1)
     fore (mask, 0, 1 << n)
-      dp[c & 1][mask][r] = 0;
+      dp[c & 1][r][mask] = 0;
 }
 
 // Answer in dp[m][0][0]
