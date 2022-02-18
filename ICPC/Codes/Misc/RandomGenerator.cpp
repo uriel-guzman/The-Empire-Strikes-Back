bfeb2d9479c09ac1d5cfcd5da6125ce36a8d34c5
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2022-02-18 10:27:28 -0600

    Random generator create graphs and trees

--- ICPC/Codes/Misc/RandomGenerator.cpp
+++ ICPC/Codes/Misc/RandomGenerator.cpp
@@ -8,9 +8,6 @@ struct Random {
     assert(n >= 0);
     vector<T> v(n);
     if (unique) {
-      for (auto& x : v)
-        x = g();
-    } else {
       set<T> st;
       for (auto& x : v) {
         do {
@@ -18,6 +15,9 @@ struct Random {
         } while (st.count(x));
         st.insert(x);
       }
+    } else {
+      for (auto& x : v)
+        x = g();
     }
     return v;
   }
@@ -72,5 +72,43 @@ struct Random {
       s += get<char>(pattern[2 * k], pattern[min<int>(2 * k + 1, pattern.size() - 1)]);
     }
     return s;
+  }
+
+  template <class W>
+  struct Edge {
+    int from, to;
+    W weight;
+
+    bool operator<(const Edge& other) const {
+      return set<int>({from, to}) < set<int>({other.from, other.to});
+    }
+  };
+
+  template <class T>
+  vector<Edge<T>> getGraph(int numNodes, int numEdges, T low = 1, T high = 1, bool uniqueEdges = false) {
+    if (uniqueEdges) {
+      long long maxNumEdges = 1LL * numNodes * (numNodes - 1) / 2LL;
+      assert(numEdges <= maxNumEdges);
+    }
+    return fillArray<Edge<T>>(numEdges, uniqueEdges, [&]() {
+      Edge<T> edge;
+      auto myPair = getArray<int>(2, 1, numNodes, true);
+      edge.from = myPair[0];
+      edge.to = myPair[1];
+      edge.weight = get<T>(low, high);
+      return edge;
+    });
+  }
+
+  template <class T>
+  vector<Edge<T>> getTree(int numNodes, T low = 1, T high = 1) {
+    int current = 2;
+    return fillArray<Edge<T>>(numNodes - 1, true, [&]() {
+      Edge<T> edge;
+      edge.from = get<int>(1, current - 1);
+      edge.to = current++;
+      edge.weight = get<T>(low, high);
+      return edge;
+    });
   }
 };
