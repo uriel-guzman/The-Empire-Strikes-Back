aff33f176783466244518da80104d7437b7cadad
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2022-02-18 10:40:46 -0600

    Random generator comments added

--- ICPC/Codes/Misc/RandomGenerator.cpp
+++ ICPC/Codes/Misc/RandomGenerator.cpp
@@ -3,11 +3,15 @@ struct Random {
 
   Random() : rng(chrono::steady_clock::now().time_since_epoch().count()) {}
 
+  // Fills a vector of type T using a generator to create a single element.
   template <class T, class Generator>
   vector<T> fillArray(int n, bool unique, Generator g) {
     assert(n >= 0);
     vector<T> v(n);
     if (unique) {
+      for (auto& x : v)
+        x = g();
+    } else {
       set<T> st;
       for (auto& x : v) {
         do {
@@ -15,13 +19,11 @@ struct Random {
         } while (st.count(x));
         st.insert(x);
       }
-    } else {
-      for (auto& x : v)
-        x = g();
     }
     return v;
   }
 
+  // Returns a random value, the default type is of the largest.
   template <class T, class Z = T>
   Z get(T low, Z high) {
     assert(low <= high);
@@ -32,6 +34,7 @@ struct Random {
     }
   }
 
+  // Returns a vector of size n, with all elements in the range [low, high].
   template <class T>
   vector<T> getArray(int n, T low, T high, bool unique = false) {
     if (unique)
@@ -43,6 +46,7 @@ struct Random {
     });
   }
 
+  // Returns a vector of strings of size n, with all strings following 'pattern' and of sizes ranging from [minLength, maxLength].
   vector<string> getStrings(int n, string pattern = "az", int minLength = 1, int maxLength = 10, bool unique = false) {
     if (unique) {
       // assert that is possible to generate n different strings
@@ -62,53 +66,16 @@ struct Random {
     });
   }
 
-  string getString(int size, string pattern = "az") {
-    sort(pattern.begin(), pattern.end());
+  // Returns a string of size n following 'pattern'. The 'pattern' needs a pair of elements, it could be multiple pairs, i.e. "acDF15" all strings
+  // will be of characters of the set {[a,c],[D,F],[1,5]}
+  string getString(int n, string pattern = "az") {
     assert(pattern.size());
-    assert(size >= 0);
+    assert(n >= 0);
     string s;
-    while (size--) {
+    while (n--) {
       int k = rng() % (pattern.size() / 2);
       s += get<char>(pattern[2 * k], pattern[min<int>(2 * k + 1, pattern.size() - 1)]);
     }
     return s;
-  }
-
-  template <class W>
-  struct Edge {
-    int from, to;
-    W weight;
-
-    bool operator<(const Edge& other) const {
-      return set<int>({from, to}) < set<int>({other.from, other.to});
-    }
-  };
-
-  template <class T>
-  vector<Edge<T>> getGraph(int numNodes, int numEdges, T low = 1, T high = 1, bool uniqueEdges = false) {
-    if (uniqueEdges) {
-      long long maxNumEdges = 1LL * numNodes * (numNodes - 1) / 2LL;
-      assert(numEdges <= maxNumEdges);
-    }
-    return fillArray<Edge<T>>(numEdges, uniqueEdges, [&]() {
-      Edge<T> edge;
-      auto myPair = getArray<int>(2, 1, numNodes, true);
-      edge.from = myPair[0];
-      edge.to = myPair[1];
-      edge.weight = get<T>(low, high);
-      return edge;
-    });
-  }
-
-  template <class T>
-  vector<Edge<T>> getTree(int numNodes, T low = 1, T high = 1) {
-    int current = 2;
-    return fillArray<Edge<T>>(numNodes - 1, true, [&]() {
-      Edge<T> edge;
-      edge.from = get<int>(1, current - 1);
-      edge.to = current++;
-      edge.weight = get<T>(low, high);
-      return edge;
-    });
   }
 };
