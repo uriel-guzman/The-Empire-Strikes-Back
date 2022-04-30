b5ee61356c748e6b515710ad8f2a9a2f3249be53
Abraham Baltazar Murillo Sandoval <36650502+abraham-murillo@users.noreply.github.com>
2022-04-30 11:20:59 -0500

    Update Factorial.cpp

--- ICPC/Codes/Math/Combinatorics/Factorial.cpp
+++ ICPC/Codes/Math/Combinatorics/Factorial.cpp
@@ -1,6 +1,6 @@
 fac[0] = 1LL;
 fore (i, 1, N)
-  fac[i] = lli(i) * fac[i - 1] % mod;
-ifac[n - 1] = fpow(fac[n - 1], mod - 2, mod);
-for (int i = N - 1; i >= 0; i--)
-  ifac[i] = lli(i + 1) * ifac[i + 1] % mod;
+  fac[i] = lli(i) * fac[i - 1] % MOD;
+ifac[N - 1] = fpow(fac[N - 1], MOD - 2, MOD);
+for (int i = N - 2; i >= 0; i--)
+  ifac[i] = lli(i + 1) * ifac[i + 1] % MOD;
