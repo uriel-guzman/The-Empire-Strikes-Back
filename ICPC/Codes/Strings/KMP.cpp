24287ba5657b3977095073ee56473bf06194d258
Pino-29 <robertogarciap29@gmail.com>
2023-06-01 23:06:53 -0600

    kMP fixed

--- ICPC/Codes/Strings/KMP.cpp
+++ ICPC/Codes/Strings/KMP.cpp
@@ -2,9 +2,9 @@ template <class T>
 vector<int> lps(T s) {
   vector<int> p(sz(s), 0);
   for (int j = 0, i = 1; i < sz(s); i++) {
-    while (j && s[i] != s[j])
+    while (j && (j == sz(s) || s[i] != s[j]))
       j = p[j - 1];
-    if (s[i] == s[j])
+    if (j < sz(s) && s[i] == s[j])
       j++;
     p[i] = j;
   }
@@ -15,10 +15,11 @@ vector<int> lps(T s) {
 template <class T>
 vector<int> kmp(T& s, T& t) {
   vector<int> p = lps(t), pos;
+  debug(lps(t), sz(s));
   for (int j = 0, i = 0; i < sz(s); i++) {
-    while (j && s[i] != t[j])
+    while (j && (j == sz(t) || s[i] != t[j]))
       j = p[j - 1];
-    if (s[i] == t[j])
+    if (j < sz(t) && s[i] == t[j])
       j++;
     if (j == sz(t))
       pos.pb(i - sz(t) + 1);
