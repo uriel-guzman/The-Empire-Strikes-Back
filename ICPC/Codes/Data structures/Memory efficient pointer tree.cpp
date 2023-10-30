ff2508263f524b0a7addd676ac140d5e89a6f812
Abraham Baltazar Murillo Sandoval <36650502+abraham-murillo@users.noreply.github.com>
2023-10-30 16:26:13 -0700

    Create Memory efficient pointer tree.cpp

---
+++ ICPC/Codes/Data structures/Memory efficient pointer tree.cpp
@@ -0,0 +1,26 @@
+struct Seg {
+  int l, r;
+  Seg *left, *right;
+  int sum;
+
+  template <class Arr>
+  Seg(int l, int r, Arr &a, vector<Seg> &mem) : l(l), r(r), left(0), right(0) {
+    if (l == r) {
+      sum = a[l];
+      return;
+    }
+    int m = (l + r) >> 1;
+    mem.emplace_back(Seg(l, m, a, mem));
+    left = &mem.back();
+    mem.emplace_back(Seg(m + 1, r, a, mem));
+    right = &mem.back();
+    sum = left->sum + right->sum;
+  }
+
+  // ...
+};
+
+vector<int> a = {1, 2, 3, 4, 5};
+vector<Seg> mem;
+mem.reserve(2 * n - 1);
+Seg tree(0, a.size() - 1, a, mem);
